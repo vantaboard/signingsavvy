456332d373183e192ac3a67cdba8ef54df2b78a7
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-01-03 08:34:49 -0800

    fix(remote.py): working through some issues with errors found from the python lsp

--- src/pysign/__main__.py
+++ src/pysign/__main__.py
@@ -1,7 +1,15 @@
-import sys
-
+from sys import exit
+import logging
+from logging import basicConfig
 
 if __name__ == "__main__":
-    from poetry.console.application import main
+  from poetry.console.application import main
 
-    sys.exit(main())
+  level = getattr(logging, loglevel.upper(), None)
+  if not isinstance(level, int):
+      raise ValueError(f"Bad log level: {level}")
+
+  basicConfig("../logs/pysign.log", encoding="utf-8", level=level)
+
+  exit(main())
+
