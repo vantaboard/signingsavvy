fadb288f72eb8d326e1f12db0dbaac048212ab13
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-01-03 09:22:15 -0800

    feat(*.coco): rename coco to py

--- src/pysign/__main__.py
+++ src/pysign/__main__.py
@@ -1,7 +1,15 @@
-import sys
-
+from sys import exit
+import logging
+from logging import basicConfig
 
 if __name__ == "__main__":
-    from poetry.console.application import main
+  from poetry.console.application import main
 
-    sys.exit(main())
+  level = (logging, loglevel.upper(), None) |*> getattr
+  if not (level, int) |*> isinstance:
+      raise f"Bad log level: {level}" |> ValueError
+
+  basicConfig(?, encoding="utf-8", level=level) <| "../logs/pysign.log"
+
+  main() |> exit
+
