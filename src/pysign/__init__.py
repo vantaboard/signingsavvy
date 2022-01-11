ca92983a21d7631bc6777c8b28b877d5b483e52c
Brighten Tompkins <brightenqtompkins@gmail.com>
2022-01-11 01:16:49 -0800

    feat(api.py): rewrote api using quart as a mimicked api

--- src/pysign/__init__.py
+++ src/pysign/__init__.py
@@ -1,18 +1,12 @@
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 # type: ignore[has-type]
 
-"""This module demonstrates basic Sphinx usage with Python modules.
-
+"""
 Submodules
 ==========
 
 .. autosummary::
     :toctree: _autosummary
 
-    db
-    start
-    anki
     api
-    db
-    interfaces
 """
--- src/pysign/types.py
+++
@@ -1,6 +0,0 @@
-"""Module for custom types."""
-
-from typing import Literal
-
-
-VideoQuality = Literal["ld", "sd", "hd"]
